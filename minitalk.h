da02b2fa02e5a5679388d599038404c5ccec49e4
jklom0326 <jklom0326@gmail.com>
2022-06-07 22:52:35 +0900

    feat.server

---
+++ minitalk.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shan <shan@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/07 22:50:31 by shan              #+#    #+#             */
+/*   Updated: 2022/06/07 22:50:31 by shan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "../libft/incs/libft.h"
+
+#endif
